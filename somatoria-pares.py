412331f4815ba8f84da8fe6b5c6c396132dbe9b5
Carlos Leonardo Goes Farias <carlos.leonardo.goes@gmail.com>
2026-08-06 14:38:31 -0300

    Altera script para solicitar entrada do usuário e calcular a soma de números pares

--- somatoria-pares.py
+++ somatoria-pares.py
@@ -1,3 +1,15 @@
 print("Soma todos os números pares de 1 a 100")
-soma = sum(i for i in range(1, 101) if i % 2 == 0)
-print(f"A soma dos números pares de 1 a 100 é: {soma}")
+try:
+	total = int(input("Informe o total de números pares que deseja somar (1 a 100): "))
+	if total < 1 or total > 100:
+		raise ValueError("O número deve estar entre 1 e 100.")	
+	
+	numeros = []
+	for i in range(1, total + 1):
+		numero = int(input(f"Digite o {i}º número: "))
+		numeros.append(numero)
+	soma = sum(num for num in numeros if num % 2 == 0)
+		
+	print(f"A soma dos {total} primeiros números pares é: {soma}")
+except ValueError as e:
+	print(f"Erro: {e}")
