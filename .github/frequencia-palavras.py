ea62006d80e565c48fcd486bdccb55d843d5a471
Carlos Leonardo Goes Farias <carlos.leonardo.goes@gmail.com>
2026-09-02 13:49:57 -0300

    Adiciona script para contagem de frequência de palavras em um texto

---
+++ .github/frequencia-palavras.py
@@ -0,0 +1,19 @@
+from tokenizer import split_into_sentences
+
+print("Contagem de Frequência de Palavras")
+while True:
+    texto = input("Digite um texto: ")
+    palavras = split_into_sentences(texto)
+    palavras = [palavra for frase in palavras for palavra in frase.split()]
+    frequencia = {}
+    for palavra in palavras:
+        if palavra in frequencia:
+            frequencia[palavra] += 1
+        else:
+            frequencia[palavra] = 1
+    for palavra, contagem in frequencia.items():
+        print(f"{palavra}: {contagem}")
+    print()
+    continuar = input("Deseja digitar outro texto? (s/n): ")
+    if continuar.lower() != 's':
+        break
